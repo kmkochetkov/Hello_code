c0143a82fc0c7598cec527623d4dce6b2b3b3c19
Konstantin Kochetkov <kmkochetkov@gmail.com>
2022-08-30 21:49:09 +0300

    Задание 2 практики

---
+++ Lesson1_002/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает
+// два числа и проверяет, является ли первое
+// число квадратом второго.
+
+Console.WriteLine("введи число 1");
+int number1=int.Parse(Console.ReadLine());
+Console.WriteLine("введи число 2");
+int number2=int.Parse(Console.ReadLine());
+
+ if (number1==number2*number2)
+ {
+     Console.WriteLine("yes");
+ }
+  else
+   {
+     Console.WriteLine("no"); 
+   } 
